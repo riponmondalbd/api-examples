994d655798ab49aaac6b0d50ae328bb95aad3ea5
Ripon Mondal <riponmondal149@gmail.com>
2023-09-06 00:08:49 +0600

    create dynamic url based on click and display modal

--- js/meels.js
+++ js/meels.js
@@ -12,7 +12,7 @@ const displayMeals = meals => {
     mealsContainer.innerHTML = '';
 
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         // step 2: create child for each element
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
@@ -23,6 +23,11 @@ const displayMeals = meals => {
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strInstructions}</p>
+
+                <!-- Button trigger modal -->
+                <button onClick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                Details
+                </button>
             </div>
         </div>
         `
@@ -35,9 +40,24 @@ const displayMeals = meals => {
 const searchMeals = () => {
     const searchText = document.getElementById('search-field').value;
     // search meals
-    console.log(searchText)
+    // console.log(searchText)
     loadMeals(searchText);
 
+}
+
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+    <img class='img-fluid' src="${meal.strMealThumb}">
+    `
 }
 
 loadMeals('rice');
