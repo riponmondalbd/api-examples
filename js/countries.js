8fe9726a363208b2dbcc342666fd77d1c4733223
Ripon Mondal <riponmondal149@gmail.com>
2023-09-05 22:37:42 +0600

    call dynamic API, load dynamic data to display

--- js/countries.js
+++ js/countries.js
@@ -17,9 +17,30 @@ const displayCountries = (countries) => {
         countryDiv.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
             <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+            <button onClick="loadCountryDetails('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     })
 
 }
+
+const loadCountryDetails = code => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showCountryDetails(data[0]))
+}
+
+const showCountryDetails = country => {
+    const countryDetailsContainer = document.getElementById('country-details');
+    countryDetailsContainer.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+        <img src="${country.flags.png}" alt="">
+    `
+}
+
 loadCountries();
